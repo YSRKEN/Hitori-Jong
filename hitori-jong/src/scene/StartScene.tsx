4554077b7c0f5b813931a0022b0462b1ccc33c97
YSRKEN <ysr.ken@gmail.com>
2019-10-12 18:15:09 +0900

    [add] 各種リンクもスタート画面に追加した

--- hitori-jong/src/scene/StartScene.tsx
+++ hitori-jong/src/scene/StartScene.tsx
@@ -5,25 +5,32 @@ import { TwitterShareButton } from 'react-share';
 
 // タイトル画面
 const StartScene: React.FC = () => (
-	<div className="title-bar">
-		<div className="title-logo">
-			<span>ヒトリジャン</span>
+	<>
+		<div className="misc-links">
+			<span><a href="https://twitter.com/YSRKEN" rel="noopener noreferrer" target="_blank">作者Twitter</a></span>
+			<span><a href="https://github.com/YSRKEN/Hitori-Jong" rel="noopener noreferrer" target="_blank">GitHub</a></span>
+			<span><a href="https://github.com/YSRKEN/Hitori-Jong/wiki/取扱説明書" rel="noopener noreferrer" target="_blank">取扱説明書</a></span>
 		</div>
-		<div className="version-logo">
-			<span>Ver.{APPLICATION_VERSION}</span>
+		<div className="title-bar">
+			<div className="title-logo">
+				<span>ヒトリジャン</span>
+			</div>
+			<div className="version-logo">
+				<span>Ver.{APPLICATION_VERSION}</span>
+			</div>
 		</div>
 		<div className="button-group">
-			<span className="button game-button">ゲーム開始</span>
-			<span className="button simulation-button">シミュレーション</span>
+			<span className="game-button">ゲーム開始</span>
+			<span className="simulation-button">シミュレーション</span>
 			<TwitterShareButton
 				url="https://hitori-jong.firebaseapp.com"
 				title="一人用ミリジャン「ヒトリジャン」。スマホ対応。"
 				hashtags={['ミリジャン', 'ヒトリジャン']}
 			>
-				<span className="button share-button">Twitter にシェア</span>
+				<span className="share-button">Twitter にシェア</span>
 			</TwitterShareButton>
 		</div>
-	</div>
+	</>
 );
 
 export default StartScene;
