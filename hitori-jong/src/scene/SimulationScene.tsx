6402cdfbd8a8e2ff1f5b7bd5b02e6a67d659f9d7
YSRKEN <ysr.ken@gmail.com>
2019-10-16 17:28:53 +0900

    [add] 各種コマンドを、チェック状況によって入れ替えられるようにした

--- hitori-jong/src/scene/SimulationScene.tsx
+++ hitori-jong/src/scene/SimulationScene.tsx
@@ -4,10 +4,51 @@ import HandTileView from 'parts/HandTileView';
 import MyIdolView from 'parts/MyIdolView';
 import './SimulationScene.css';
 import Button from 'parts/Button';
+import { UNIT_LIST2 } from 'constant/unit';
 
 // シミュレーション画面
 const SimulationScene: React.FC = () => {
-  const { simulationHand, dispatch } = React.useContext(StateContext);
+  const { simulationHand, handCheckFlg, dispatch } = React.useContext(
+    StateContext,
+  );
+
+  let commandSet: JSX.Element;
+  if (handCheckFlg.findIndex(flg => flg) < 0) {
+    // いずれのチェックボックスも選択していない場合
+    commandSet = (
+      <>
+        <Button text="ユニット検索" />
+        <Button text="受け入れ検索" />
+      </>
+    );
+  } else {
+    // チェック位置にいずれのユニットも存在しないか？
+    const allUnitMemberCount =
+      simulationHand.unitIndexes.length > 0
+        ? simulationHand.unitIndexes
+            .map(index => UNIT_LIST2[index].memberCount)
+            .reduce((p, c) => p + c)
+        : 0;
+    if (handCheckFlg.slice(0, allUnitMemberCount).findIndex(flg => flg) < 0) {
+      // チェック位置にいずれのユニットも存在しない
+      commandSet = (
+        <>
+          <Button text="チー" />
+          <Button text="ユニット固定" />
+          <Button text="左シフト" />
+          <Button text="右シフト" />
+        </>
+      );
+    } else {
+      // チェック位置にいずれかのユニットが存在する
+      commandSet = (
+        <>
+          <Button text="チー解除" />
+          <Button text="ユニット固定解除" />
+        </>
+      );
+    }
+  }
 
   const onClickStoT = () => dispatch({ type: 'changeSceneStoT', message: '' });
 
@@ -21,14 +62,7 @@ const SimulationScene: React.FC = () => {
         />
         <MyIdolView />
       </div>
-      <div className="command-button-group">
-        <Button text="チー／解除" />
-        <Button text="ユニット固定／解除" />
-        <Button text="左シフト" />
-        <Button text="右シフト" />
-        <Button text="ユニット検索" />
-        <Button text="受け入れ検索" />
-      </div>
+      <div className="command-button-group">{commandSet}</div>
       <div className="footer-hand-tile-view">
         <HandTileView hand={simulationHand} />
       </div>
