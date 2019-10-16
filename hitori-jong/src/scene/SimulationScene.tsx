f0de9afa8e73f65df6188e8c24151f8c4f806d70
YSRKEN <ysr.ken@gmail.com>
2019-10-16 17:41:02 +0900

    [change] JSX生成部分を関数として括った

--- hitori-jong/src/scene/SimulationScene.tsx
+++ hitori-jong/src/scene/SimulationScene.tsx
@@ -12,26 +12,24 @@ const SimulationScene: React.FC = () => {
     StateContext,
   );
 
-  let commandSet: JSX.Element;
-  if (handCheckFlg.findIndex(flg => flg) < 0) {
+  // 各種コマンド
+  const onClickStoT = () => dispatch({ type: 'changeSceneStoT', message: '' });
+
+  // コマンドパレットを動的生成
+  const calcCommandJsxElement = (): JSX.Element => {
     // いずれのチェックボックスも選択していない場合
-    commandSet = (
-      <>
-        <Button text="ユニット検索" />
-        <Button text="受け入れ検索" />
-      </>
-    );
-  } else {
-    // チェック位置にいずれのユニットも存在しないか？
-    const allUnitMemberCount =
-      simulationHand.unitIndexes.length > 0
-        ? simulationHand.unitIndexes
-            .map(index => UNIT_LIST2[index].memberCount)
-            .reduce((p, c) => p + c)
-        : 0;
-    if (handCheckFlg.slice(0, allUnitMemberCount).findIndex(flg => flg) < 0) {
-      // チェック位置にいずれのユニットも存在しない
-      commandSet = (
+    if (handCheckFlg.findIndex(flg => flg) < 0) {
+      return (
+        <>
+          <Button text="ユニット検索" />
+          <Button text="受け入れ検索" />
+        </>
+      );
+    }
+
+    // いずれのユニットも存在しない場合
+    if (simulationHand.unitIndexes.length === 0) {
+      return (
         <>
           <Button text="チー" />
           <Button text="ユニット固定" />
@@ -39,18 +37,32 @@ const SimulationScene: React.FC = () => {
           <Button text="右シフト" />
         </>
       );
-    } else {
-      // チェック位置にいずれかのユニットが存在する
-      commandSet = (
+    }
+
+    // チェック位置にいずれのユニットも存在しない場合
+    const allUnitMemberCount = simulationHand.unitIndexes
+      .map(index => UNIT_LIST2[index].memberCount)
+      .reduce((p, c) => p + c);
+    if (handCheckFlg.slice(0, allUnitMemberCount).findIndex(flg => flg) < 0) {
+      // チェック位置にいずれのユニットも存在しない
+      return (
         <>
-          <Button text="チー解除" />
-          <Button text="ユニット固定解除" />
+          <Button text="チー" />
+          <Button text="ユニット固定" />
+          <Button text="左シフト" />
+          <Button text="右シフト" />
         </>
       );
     }
-  }
 
-  const onClickStoT = () => dispatch({ type: 'changeSceneStoT', message: '' });
+    // チェック位置にいずれかのユニットが存在する場合
+    return (
+      <>
+        <Button text="チー解除" />
+        <Button text="ユニット固定解除" />
+      </>
+    );
+  };
 
   return (
     <>
@@ -62,7 +74,7 @@ const SimulationScene: React.FC = () => {
         />
         <MyIdolView />
       </div>
-      <div className="command-button-group">{commandSet}</div>
+      <div className="command-button-group">{calcCommandJsxElement()}</div>
       <div className="footer-hand-tile-view">
         <HandTileView hand={simulationHand} />
       </div>
