6d7ac6e9d4eb3fb5afc39e9a5fca912a07919910
YSRKEN <ysr.ken@gmail.com>
2019-10-17 15:55:37 +0900

    [add] アイドル選択画面で、アイドルのタイルをちゃんと表示できるようにした

--- hitori-jong/src/scene/IdolSelectScene.tsx
+++ hitori-jong/src/scene/IdolSelectScene.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Button from 'parts/Button';
 import StateContext from 'context';
+import { KANA_TO_IDOL_LIST, IDOL_LIST } from 'constant/idol';
+import './IdolSelectScene.css';
 
 // アイドル選択画面
 const IdolSelectScene: React.FC = () => {
-  const { dispatch } = React.useContext(StateContext);
+  const { selectedKana, dispatch } = React.useContext(StateContext);
 
   // 各種コマンド
   const onClickItoS = () => dispatch({ type: 'changeSceneItoS', message: '' });
@@ -17,6 +19,25 @@ const IdolSelectScene: React.FC = () => {
           text="シミュレーション画面に戻る"
           onClick={onClickItoS}
         />
+      </div>
+      <div className="idol-tile-list">
+        {KANA_TO_IDOL_LIST.filter(r => r.kana === selectedKana)[0].idol.map(
+          idolIndex => {
+            const idol = IDOL_LIST[idolIndex];
+            const fontStyle =
+              idol.name.length >= 4 ? 'font-size-small' : 'font-size-normal';
+
+            return (
+              <button
+                type="button"
+                className={`idol-tile color-${idol.type} ${fontStyle}`}
+                key={idolIndex}
+              >
+                {idol.name}
+              </button>
+            );
+          },
+        )}
       </div>
     </>
   );
